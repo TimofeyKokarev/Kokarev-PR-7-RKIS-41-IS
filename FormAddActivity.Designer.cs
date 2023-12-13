e030b794d87e5097d94bbeb435ad9e4f072485d2
kokti <kokti@DESKTOP-O4BLO1L>
2023-12-13 15:59:25 +0300

    Добавьте файлы проекта.

---
+++ FormAddActivity.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace KokarevPR7
+{
+    partial class FormAddActivity
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.eventPlanIDComboBox = new System.Windows.Forms.ComboBox();
+            this.DateTextBox = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.moderatorComboBox = new System.Windows.Forms.ComboBox();
+            this.juriComboBox = new System.Windows.Forms.ComboBox();
+            this.buttonAddJuri = new System.Windows.Forms.Button();
+            this.buttonAddActivity = new System.Windows.Forms.Button();
+            this.eventBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.userBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.userBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.eventBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonBack.Location = new System.Drawing.Point(12, 12);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(105, 40);
+            this.buttonBack.TabIndex = 5;
+            this.buttonBack.Text = "Назад";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(186, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Добавление новых активностей";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(150, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 31);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Название";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(92, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(191, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "План события";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(208, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 31);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "День";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(6, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(277, 31);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Начало мероприятия";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(136, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(147, 31);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Модератор";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(186, 360);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(92, 31);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Жюри";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleTextBox.Location = new System.Drawing.Point(289, 132);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(305, 39);
+            this.titleTextBox.TabIndex = 13;
+            // 
+            // eventPlanIDComboBox
+            // 
+            this.eventPlanIDComboBox.DataSource = this.eventBindingSource;
+            this.eventPlanIDComboBox.DisplayMember = "Title";
+            this.eventPlanIDComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.eventPlanIDComboBox.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.eventPlanIDComboBox.FormattingEnabled = true;
+            this.eventPlanIDComboBox.Location = new System.Drawing.Point(289, 177);
+            this.eventPlanIDComboBox.Name = "eventPlanIDComboBox";
+            this.eventPlanIDComboBox.Size = new System.Drawing.Size(305, 39);
+            this.eventPlanIDComboBox.TabIndex = 15;
+            this.eventPlanIDComboBox.ValueMember = "ID";
+            // 
+            // DateTextBox
+            // 
+            this.DateTextBox.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DateTextBox.Location = new System.Drawing.Point(289, 222);
+            this.DateTextBox.Name = "DateTextBox";
+            this.DateTextBox.Size = new System.Drawing.Size(305, 39);
+            this.DateTextBox.TabIndex = 14;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker.Location = new System.Drawing.Point(289, 267);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(305, 39);
+            this.dateTimePicker.TabIndex = 16;
+            // 
+            // moderatorComboBox
+            // 
+            this.moderatorComboBox.DataSource = this.userBindingSource;
+            this.moderatorComboBox.DisplayMember = "Surname";
+            this.moderatorComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.moderatorComboBox.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.moderatorComboBox.FormattingEnabled = true;
+            this.moderatorComboBox.Location = new System.Drawing.Point(289, 312);
+            this.moderatorComboBox.Name = "moderatorComboBox";
+            this.moderatorComboBox.Size = new System.Drawing.Size(305, 39);
+            this.moderatorComboBox.TabIndex = 17;
+            this.moderatorComboBox.ValueMember = "ID";
+            // 
+            // juriComboBox
+            // 
+            this.juriComboBox.DataSource = this.userBindingSource2;
+            this.juriComboBox.DisplayMember = "Surname";
+            this.juriComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.juriComboBox.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.juriComboBox.FormattingEnabled = true;
+            this.juriComboBox.Location = new System.Drawing.Point(289, 357);
+            this.juriComboBox.Name = "juriComboBox";
+            this.juriComboBox.Size = new System.Drawing.Size(305, 39);
+            this.juriComboBox.TabIndex = 18;
+            this.juriComboBox.ValueMember = "ID";
+            // 
+            // buttonAddJuri
+            // 
+            this.buttonAddJuri.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAddJuri.Location = new System.Drawing.Point(600, 357);
+            this.buttonAddJuri.Name = "buttonAddJuri";
+            this.buttonAddJuri.Size = new System.Drawing.Size(145, 40);
+            this.buttonAddJuri.TabIndex = 19;
+            this.buttonAddJuri.Text = "Добавить";
+            this.buttonAddJuri.UseVisualStyleBackColor = true;
+            this.buttonAddJuri.Click += new System.EventHandler(this.buttonAddJuri_Click);
+            // 
+            // buttonAddActivity
+            // 
+            this.buttonAddActivity.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonAddActivity.Location = new System.Drawing.Point(289, 402);
+            this.buttonAddActivity.Name = "buttonAddActivity";
+            this.buttonAddActivity.Size = new System.Drawing.Size(305, 40);
+            this.buttonAddActivity.TabIndex = 20;
+            this.buttonAddActivity.Text = "Добавить активность";
+            this.buttonAddActivity.UseVisualStyleBackColor = true;
+            this.buttonAddActivity.Click += new System.EventHandler(this.buttonAddActivity_Click);
+            // 
+            // eventBindingSource
+            // 
+            this.eventBindingSource.DataSource = typeof(KokarevPR7.DBCon.Event);
+            // 
+            // userBindingSource
+            // 
+            this.userBindingSource.DataSource = typeof(KokarevPR7.DBCon.User);
+            // 
+            // userBindingSource2
+            // 
+            this.userBindingSource2.DataSource = typeof(KokarevPR7.DBCon.User);
+            // 
+            // FormAddActivity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 462);
+            this.Controls.Add(this.buttonAddActivity);
+            this.Controls.Add(this.buttonAddJuri);
+            this.Controls.Add(this.juriComboBox);
+            this.Controls.Add(this.moderatorComboBox);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.eventPlanIDComboBox);
+            this.Controls.Add(this.DateTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonBack);
+            this.Name = "FormAddActivity";
+            this.Text = "FormAddActivity";
+            this.Load += new System.EventHandler(this.FormAddActivity_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.userBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.ComboBox eventPlanIDComboBox;
+        private System.Windows.Forms.TextBox DateTextBox;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.ComboBox moderatorComboBox;
+        private System.Windows.Forms.ComboBox juriComboBox;
+        private System.Windows.Forms.Button buttonAddJuri;
+        private System.Windows.Forms.Button buttonAddActivity;
+        private System.Windows.Forms.BindingSource eventBindingSource;
+        private System.Windows.Forms.BindingSource userBindingSource;
+        private System.Windows.Forms.BindingSource userBindingSource2;
+    }
+}
